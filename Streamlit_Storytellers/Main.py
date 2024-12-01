95eb3f43aaf19bf4c2d86752d3587c4923e7f354
NADJILEM Bégoto <jnadjilembegoto@gmail.com>
2024-12-01 09:29:24 +0100

    Update Main.py

--- Streamlit_Storytellers/Main.py
+++ Streamlit_Storytellers/Main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from Streamlit_Storytellers.Pages_utiles.About_us  import about_us_page
-from Streamlit_Storytellers.Pages_utiles.Dashboard_pop_active import dash_pop_active
-from Streamlit_Storytellers.Pages_utiles.Accueil import accueil_load
+from Pages_utiles.About_us  import about_us_page
+from Pages_utiles.Dashboard_pop_active import dash_pop_active
+from Pages_utiles.Accueil import accueil_load
 import altair as alt
 import openpyxl
 import os# utiliser pour le chemin d'accès
