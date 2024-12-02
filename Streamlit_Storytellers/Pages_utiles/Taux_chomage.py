b5baf7cc327a5392ae3ade902b72ae9596f4e16f
NADJILEM Bégoto <jnadjilembegoto@gmail.com>
2024-12-02 11:55:14 +0100

    Update Taux_chomage.py

--- Streamlit_Storytellers/Pages_utiles/Taux_chomage.py
+++ Streamlit_Storytellers/Pages_utiles/Taux_chomage.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 #import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.express as px
 from Datas.data_link import data_dir
 
